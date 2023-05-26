0444761cea943fa33215070e0883716789ca7d23
Camila Souza <camilamaiara@hotmail.com.br>
2023-05-26 12:40:38 -0300

    testing routes api

--- tests/Feature/Api/UserTest.php
+++ tests/Feature/Api/UserTest.php
@@ -5,7 +5,9 @@ namespace Tests\Feature\Api;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
+use RuntimeException;
 use Tests\TestCase;
 
 class UserTest extends TestCase
@@ -24,13 +26,13 @@ class UserTest extends TestCase
      */
     public function test_it_gets_users_list()
     {
-        $users = User::factory()
+        User::factory()
             ->count(1)
             ->create();
 
         $response = $this->getJson(route('users.all'));
 
-        $response->assertOk()->assertSee($users[0]->email);
+        $response->assertStatus(401);
     }
 
     /**
@@ -106,26 +108,47 @@ class UserTest extends TestCase
         $response->assertStatus(405);
     }
 
+    public function token(): string
+    {
+
+        $email = fake()->unique()->safeEmail();
+        $userFake = [
+            'email' => $email,
+            'password' => 'admin',
+            'name' => 'admin',
+        ];
+        User::create(
+            $userFake
+        );
+
+        $response       = $this->postJson( 'api/v1/login', ['email' => $userFake['email'], 'password' => $userFake['password']]);
+        $content        = json_decode($response->getContent());
+
+        // dd($content->authorization->token);
+        if (!isset($content->authorization)) {
+            throw new RuntimeException('Token missing in response');
+        }
+
+
+        return $content->authorization->token;
+    }
+
     public function test_api_route_users() {
-        $this->json('get', 'api/v1/users')
-         ->assertStatus(Response::HTTP_OK)
-         ->assertJsonStructure(
-             [
-                 'data' => [
-                     '*' => [
-                         'id',
-                         'name',
-                         'email',
-                         'email_verified_at',
-                         'created_at',
-                         'wallet' => [
-                             'id',
-                             'balance'
-                         ]
-                     ]
-                 ]
-             ]
-         );
-  }
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '. $this->token(),
+            'Accept' => 'application/json'
+        ])->get('api/v1/users', []);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_api_route_users_id() {
+        $user = User::first();
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '. $this->token(),
+            'Accept' => 'application/json'
+        ])->post('api/v1/users/'.$user->id, []);
+
+        $response->assertStatus(200);
     }
 }
